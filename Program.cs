6798dd2644db00d9f1b1ae06ec5abfdadcf51b02
“samiksha11jain” <samiksha11jain@gmail.com>
2022-01-17 21:31:49 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineEquation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Geometry line");
+        }
+    }
+}
